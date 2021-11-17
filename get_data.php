b941b5c06dd7528a07af4c0977fe89696f0ddbdf
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-17 20:59:23 +0000

    post get latitude

--- get_data.php
+++ get_data.php
@@ -1,6 +1,7 @@
 <?php
   // The global $_POST variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
-  $data = htmlspecialchars($_GET['data']);
-  echo  $data;
+$data = htmlspecialchars($_POST['latitude']);
+echo  $data;
+
 ?>
