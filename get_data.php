0e63adf64f2767e03e83bf8a72825dc592cd7fab
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-17 21:48:21 +0000

    datafix

--- get_data.php
+++ get_data.php
@@ -1,8 +1,8 @@
 <?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-$data = htmlspecialchars($_POST['data']);
+// The global $_POST variable allows you to access the data sent with the POST method by name
+// To access the data sent with the GET method, you can use $_GET
+  $data = htmlspecialchars($_POST['data']);
 
-echo $data
+  echo $data
 
 ?>
