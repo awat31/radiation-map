349174d1bdf56023c5e46f162b5f4fd90501871a
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-17 21:52:10 +0000

    fix index

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var div = document.getElementById("latitude");
-  var latitude = div.textContent;
-  const uluru = { lat: latitude, lng: 131.036 };
+  const uluru = { lat: -25.344, lng: 131.036 };
   // The map, centered at Uluru
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
