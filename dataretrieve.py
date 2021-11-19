755e3e9fcca2a10b0e199478b32374ca5a22000a
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-19 16:32:20 +0000

    pass to js file

--- dataretrieve.py
+++ dataretrieve.py
@@ -2,11 +2,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('./index.js/initMap(latitude, longitude)', methods=['POST'])
+@app.route('./index.js', methods=['POST'])
 def result():
     print(request.data)  # raw data
     print(request.json)  # json (if content-type of application/json is sent with the request)
     print(request.get_json(force=True)) # json (if content-type of application/json is not sent)
     latitude = -12.543
     longitude = 131.036
-    return latitude, longitude
+    return render_template('index.js', latitude, longitude)
