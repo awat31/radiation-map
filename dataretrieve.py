ddbbae5b6d01ed13a207bf43df238f834a02eee5
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-19 16:57:16 +0000

    does this pass variables?

--- dataretrieve.py
+++ dataretrieve.py
@@ -2,11 +2,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('./index.js', methods=['POST'])
+@app.route('/', methods=['POST'])
 def result():
     print(request.data)  # raw data
     print(request.json)  # json (if content-type of application/json is sent with the request)
     print(request.get_json(force=True)) # json (if content-type of application/json is not sent)
-    latitude = -12.543
-    longitude = 131.036
-    return render_template('index.js', latitude, longitude)
+    data = { 'latitude': '-12.543', 'longitude': '131.036' }
+    return render_template('index.html', data=data)
