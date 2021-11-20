f76af635eac422d17a9367c5e18f164f09f39d96
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-20 21:06:14 +0000

    vwlnvjn

--- dataretrieve.py
+++ dataretrieve.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('./index.js', methods=['POST'])
 def result():
-    print(request.data)  # raw data
-    print(request.json)  # json (if content-type of application/json is sent with the request)
-    print(request.get_json(force=True)) # json (if content-type of application/json is not sent)
-    data = { 'latitude': '-12.543', 'longitude': '131.036' }
+    #print(request.data)  # raw data
+    #print(request.json)  # json (if content-type of application/json is sent with the request)
+    #print(request.get_json(force=True)) # json (if content-type of application/json is not sent)
+    data = { 'latitude': -12.543, 'longitude': 131.036 }
+    print(data)
     return render_template('./index.js', data=data)
