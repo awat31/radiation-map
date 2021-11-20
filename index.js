86b228de640fe29f495d480a446fc9312082c6dd
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-20 21:31:13 +0000

    vars?

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  //var latitude = document.getElementById("latitude");
-  //var longitude = div.textContent;
-  //var latitude = document.getElementById("longitude");
-  //var longitude = div.textContent;
+  var latitude = document.getElementById("latitude");
+  var longitude = div.textContent;
+  var latitude = document.getElementById("longitude");
+  var longitude = div.textContent;
   const uluru = { lat: 0.00, lng: 0.00 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
