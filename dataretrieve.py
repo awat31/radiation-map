ad018aa43be327ce47db36b4b5f1436b3c1f0296
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-20 20:36:55 +0000

    alvk

--- dataretrieve.py
+++ dataretrieve.py
@@ -7,6 +7,5 @@ def result():
     print(request.data)  # raw data
     print(request.json)  # json (if content-type of application/json is sent with the request)
     print(request.get_json(force=True)) # json (if content-type of application/json is not sent)
-    # data = { 'latitude': '-12.543', 'longitude': '131.036' }
-    data = request.json
+    data = { 'latitude': '-12.543', 'longitude': '131.036' }
     return render_template('index.html', data=data)
