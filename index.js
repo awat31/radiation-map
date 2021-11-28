d774ce654c3675f830266091b7410a35f43c62f4
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-28 18:45:58 +0000

    No Terrain

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: new google.maps.LatLng(55.953346, -3.188375),
-    mapTypeId: "terrain",
   });
 
   map.data.loadGeoJson(
