343737603cf587141bf3bea9130cdde8da247866
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-28 13:41:30 +0000

    ts

--- index.ts
+++ index.ts
@@ -1,19 +1,32 @@
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      zoom: 4,
-      center: uluru,
-    }
-  );
+let map: google.maps.Map;
 
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
+function initMap(): void {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8, -187.3),
+    mapTypeId: "terrain",
   });
+
+  // Create a <script> tag and set the USGS URL as the source.
+  const script = document.createElement("script");
+
+  // This example uses a local copy of the GeoJSON stored at
+  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+  script.src =
+    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
 }
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+const eqfeed_callback = function (results: any) {
+  for (let i = 0; i < results.features.length; i++) {
+    const coords = results.features[i].geometry.coordinates;
+    const latLng = new google.maps.LatLng(coords[1], coords[0]);
+
+    new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+  }
+};
