0e2fcfe2cc42762c8eedb42459cdd3265ba4096b
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-28 17:28:48 +0000

    TryNew

--- index.js
+++ index.js
@@ -7,26 +7,8 @@ function initMap() {
     mapTypeId: "terrain",
   });
 
-  // Create a <script> tag and set the USGS URL as the source.
-  const script = document.createElement("script");
-
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src =
-    "./coords.geojson";
-  document.getElementsByTagName("head")[0].appendChild(script);
+  map.data.loadGeoJson(
+    "./coords.geojson"
+  );
+  
 }
-
-// Loop through the results array and place a marker for each
-// set of coordinates.
-const eqfeed_callback = function (results) {
-  for (let i = 0; i < results.features.length; i++) {
-    const coords = results.features[i].geometry.coordinates;
-    const latLng = new google.maps.LatLng(coords[1], coords[0]);
-
-    new google.maps.Marker({
-      position: latLng,
-      map: map,
-    });
-  }
-};
