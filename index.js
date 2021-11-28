cd22b9399098edd8ff7a7dd2a34e57744e227660
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-28 13:44:52 +0000

    move starter

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 2,
-    center: new google.maps.LatLng(2.8, -187.3),
+    center: new google.maps.LatLng(8, 0),
     mapTypeId: "terrain",
   });
 
