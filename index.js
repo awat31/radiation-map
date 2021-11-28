286c8f5e8a22d0ed1a8e744bb0311a5751bab373
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-28 13:50:04 +0000

    Zoom In

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 5,
+    zoom: 6,
     center: new google.maps.LatLng(55.953346, -3.188375),
     mapTypeId: "terrain",
   });
