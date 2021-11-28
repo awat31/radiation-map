aafeb7f5a517af6b265ae8857bfe8f805293385a
Aaron Watkins <watkins.aaron2@gmail.com>
2021-11-28 13:49:17 +0000

    better start

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: new google.maps.LatLng(0.00, 0.00),
+    zoom: 5,
+    center: new google.maps.LatLng(55.953346, -3.188375),
     mapTypeId: "terrain",
   });
 
