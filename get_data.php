7d804a8e2becbe203a8e0cb19fd5b6fd2453cd5d
Aaron Watkins <watkins.aaron2@gmail.com>
2021-12-01 02:27:25 +0000

    Pull Data from SQL- 2

--- get_data.php
+++
@@ -1,8 +0,0 @@
-<?php
-// The global $_POST variable allows you to access the data sent with the POST method by name
-// To access the data sent with the GET method, you can use $_GET
-  $data = htmlspecialchars($_POST['data']);
-
-  echo $data
-
-?>
